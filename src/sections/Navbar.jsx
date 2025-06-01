fd6ce46c61b5553e75de2945bb646705c8db68c5
Newt-Shadow <anmol.s.sahoo@gmail.com>
2025-06-01 20:59:01 +0530

    Uno

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             href="/"
             className="text-xl font-bold transition-colors text-neutral-400 hover:text-white"
           >
-            Ali
+            N/S
           </a>
           <button
             onClick={() => setIsOpen(!isOpen)}
